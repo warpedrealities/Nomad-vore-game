d263e4b226c454f263e965063b4d62dc88a128c3
warped realities <chrisofit113@hotmail.co.uk>
2018-01-17 21:54:54 +0000

    raptor fixed

--- assets/data/ai/raptor.lua
+++ assets/data/ai/raptor.lua
@@ -25,18 +25,20 @@ end
 
 function pursuit(controllable,sense,script)
 	actor=sense:getPlayer(controllable,true)
-	if not (actor == nil and  pos:getDistance(me)<8) then
+	
+	if not (actor == nil) then
 		pos=actor:getPosition()
 		me=controllable:getPosition()
-		if (pos:getDistance(me)>1.5) then
+		if (pos:getDistance(me)>1.5 and pos:getDistance(me)<8) then
 			if controllable:HasPath() then
 				controllable:FollowPath()
 			else
 				controllable:Pathto(pos.x,pos.y)
 			end	
-		else
-			controllable:setValue(3,0) 
-			controllable:startConversation()
+		else 
+			if (pos:getDistance(me)<1.5) then
+				controllable:startConversation()
+			end
 		end
 	else
 	stationKeeping(controllable)
