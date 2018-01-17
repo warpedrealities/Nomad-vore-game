65d4c5e1e5aaf9e2382f3809eaecc990e4efd8a3
warped realities <chrisofit113@hotmail.co.uk>
2018-01-17 21:37:46 +0000

    raptor added

--- assets/data/ai/raptor.lua
+++ assets/data/ai/raptor.lua
@@ -1,68 +1,54 @@
 
-
-function hide(controllable,sense,pos)
-	controllable:specialCommand("hide")
-
-end
-
-function aggressive(controllable,sense,pos,hostile)
-
-
-	if pos:getDistance(hostile:getPosition())<2 then
-	--if in melee range attack
-	controllable:Attack(hostile:getPosition().x,hostile:getPosition().y)
-
-	else
-	--if not move towards player
-	if controllable:HasPath() then
-		controllable:FollowPath()
-	else
-		controllable:Pathto(hostile:getPosition().x,hostile:getPosition().y)
-			controllable:FollowPath()	
+function stationKeeping(controllable)
+	pos=controllable:getPosition()
+	a=controllable:getValue(0)
+	b=controllable:getValue(1)
+	if (a==0) then
+		a=pos.x
 	end
+	if (b==0) then
+		b=pos.y
 	end
-
-end
-
-function combat(controllable,sense,pos,script,hostile)
-a=script:getValue(0)
-b=controllable:getHealth()
-c=controllable:getValue(0)
-c=c+1
-controllable:setValue(0)
-a=a+1
-script:setValue(0,a)
-if (a>3 or b<20 or c>10) then
-	aggressive(controllable,sense,pos,hostile)
-
-else
-
-	if controllable:HasPath() then
-		controllable:FollowPath()
-	else
-		hide(controllable,sense,pos)
+	
+	controllable:setValue(0,a)
+	controllable:setValue(1,b)
+	
+	if ( not a==pos.x and not b==pos.y ) then
+		if controllable:HasPath() then
+			controllable:FollowPath()
+		else
+			controllable:Pathto(a,b)
+		end
 	end
 end
 
+
+function pursuit(controllable,sense,script)
+	actor=sense:getPlayer(controllable,true)
+	if not (actor == nil and  pos:getDistance(me)<8) then
+		pos=actor:getPosition()
+		me=controllable:getPosition()
+		if (pos:getDistance(me)>1.5) then
+			if controllable:HasPath() then
+				controllable:FollowPath()
+			else
+				controllable:Pathto(pos.x,pos.y)
+			end	
+		else
+			controllable:setValue(3,0) 
+			controllable:startConversation()
+		end
+	else
+	stationKeeping(controllable)
+	end
+
 end
+
 
 function main(controllable, sense, script)  
-	pos=controllable:getPosition()
-	hostile=sense:getHostile(controllable,10,true)
-	if not (hostile == nil ) and not controllable:getPeace() then
-	--combat ai here
-	combat(controllable,sense,pos,script,hostile)
-
+	if (controllable:getRPG():hasStatus(23)==false) then
+		controllable:useSelfMove(0)
 	else
-	a=math.random(0,8)
-	controllable:move(a);
+		pursuit(controllable,sense,script)
 	end
 end  
-
-function tick(script)
-a=script:getValue(0)
-if (a>0) then
-	a=a-1
-	script:setValue(0,a)
-end
-end
