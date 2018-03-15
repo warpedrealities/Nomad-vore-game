716732bab366d02fdd567c6ac98e27446c8f7d7d
warped realities <chrisofit113@hotmail.co.uk>
2018-03-15 18:54:24 +0000

    update 271A

--- assets/data/ai/raptorHuntress.lua
+++ assets/data/ai/raptorHuntress.lua
@@ -37,11 +37,11 @@ function passive(controllable,sense, pos)
 
 	hostile=sense:getHostile(controllable,10,true)
 	if not (hostile == nil ) and not controllable:getPeace() then
-	if controllable:HasPath() then
-		controllable:FollowPath()
-	else
-		hide(controllable,sense,pos)
-	end	
+		if controllable:HasPath() then
+			controllable:FollowPath()
+		else
+			hide(controllable,sense,pos)
+		end	
 	else
 	a=math.random(0,8)
 	controllable:move(a);	
@@ -49,29 +49,23 @@ function passive(controllable,sense, pos)
 end
 
 function control(controllable,sense,pos,script)
-a=script:getShared():getValue(0)
-b=controllable:getHealth()
-c=controllable:getValue(0)
-c=c+1
-controllable:setValue(0)
-a=a+1
-script:setValue(0,a)
-if (a==1 or b<30 or c>10) then
-	aggressive(controllable,sense,pos)
+	a=script:getShared():getValue(0)
+	b=controllable:getHealth()
+	c=controllable:getValue(0)
+	c=c+1
+	controllable:setValue(0)
+	a=a+1
+	script:setValue(0,a)
+	if (a==1 or b<30 or c>10) then
+		aggressive(controllable,sense,pos)
 
-else
-	passive(controllable,sense,pos)
+	else
+		passive(controllable,sense,pos)
 
-end
-
+	end
 end
 
 function main(controllable, sense, script)  
 	pos=controllable:getPosition()
 	control(controllable,sense,script)
-	else
-
-	end
 end  
-
-end
