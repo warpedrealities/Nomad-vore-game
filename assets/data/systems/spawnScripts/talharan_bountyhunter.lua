7f2b0021b6745e5dedf2432f56d126c55c162ca4
warped realities <chrisofit113@hotmail.co.uk>
2019-07-21 19:45:41 +0100

    fix for bounty hunter spawn script

--- assets/data/systems/spawnScripts/talharan_bountyhunter.lua
+++ assets/data/systems/spawnScripts/talharan_bountyhunter.lua
@@ -1,7 +1,6 @@
 
 function main(tools)  
-	a=math.random(0,8)
-	if (a<2 and tools:getFactions():getFaction("talharan"):getRelationship("player") <= 50) then	
+	if (tools:getFactions():getFaction("talharan"):getRelationship("player") <= 50) then	
 		if (tools:countFaction("pirate")==0 and 
 		tools:getFactions():getFaction("talharan"):getFactionFlags():readFlag("bountyhunter") == 0 ) then
 			tools:spawn("fighter","talharan_bountyhunter")
